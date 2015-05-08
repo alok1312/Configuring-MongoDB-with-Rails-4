ba818756edaf1d1f68081a19d2418c5447b561f0
alok1312 <alok.kumar@mavs.uta.edu>
2015-05-08 01:51:22 -0700

    Initial commit

---
+++ app/controllers/alok_controller.rb
@@ -0,0 +1,26 @@
+require 'mongo'
+require 'open-uri'
+class AlokController < ApplicationController
+  def index
+  	mongo_client = Mongo::MongoClient.new("localhost", 27017)
+  	db = mongo_client.db("mydb")
+  	coll = db.collection("testCollection")
+  	coll = db["testCollection"]
+  	
+    json = open('MOCK_DATA.json') { |f| f.read }
+
+  	jsonData = JSON.parse(json)
+    
+    jsonData.each do |variable|
+	   coll.insert(variable)
+    end
+  end
+  def getDbData
+    mongo_client = Mongo::MongoClient.new("localhost", 27017)
+    db = mongo_client.db("mydb")
+  	coll = db.collection("testCollection")
+  	coll = db["testCollection"]
+    retrieve = coll.find();
+
+  end
+end
